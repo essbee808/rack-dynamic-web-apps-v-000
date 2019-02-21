1e425c6c28a8dccada166416e2c68395707ec2fc
Sheena Bonilla <sheena.myb@gmail.com>
2019-02-21 17:14:43 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,10 @@ class Application
     resp = Rack::Response.new
     
     num_1 = Kernel.rand(1..20)
+    num_2 = Kernel.rand(1..20)
+    num_3 = Kernel.rand(1..20)
     
+    if num_1 == num_2 && num_2 == num_3
     resp.write "Hello, World"
     resp.finish
   end
